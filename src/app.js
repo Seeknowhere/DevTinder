ed363484bf77dc6bf98ae4ad57c1b59a0723867b
Shaira <mallari.shaira31@gmail.com>
2025-10-29 16:10:08 +0800

    add schema types

--- src/app.js
+++ src/app.js
@@ -1,35 +1,70 @@
-const { adminAuth, userAuth } = require("./middlewares/auth.js")
-const express = require('express');
-const connectDB = require("./config/database")
-const User = require('./models/user');
+const { adminAuth, userAuth } = require("./middlewares/auth.js");
+const express = require("express");
+const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express();
 
 app.use(express.json());
 
-app.post("/signup",async (req, res) => {
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
 
-    const user = new User(req.body);
-   
-    try {
-        await user.save();
-        res.send("User added successfully")
-    } catch (err) {
-        res.status(400).send("Error saving the user:" + err.message)
+  try {
+    await user.save();
+    res.send("User added successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if(!users || users === 0) {
+        res.status(404).send("User not found");
+    }else {
+        const nameList = users.map((user) => {
+             return `<li>${user.firstName} : ${user.age}</li>`
+        }).join('')
+
+        res.send(`<h1>Found Users:</h1><ul>${nameList}</ul>`)
     }
-    
+   
+  } catch (err) {
+    res.status(400).send("Something went wrong:" + err.message);
+  }
+});
 
+app.delete("/user", async(req,res)=>{
+    const userId = req.body.userId;
+    try {
+        const users = await User.findByIdAndDelete(userId);
+        res.send(users.firstName + " " + users.lastName)
+    }catch(err){
+        res.status(404).send("not found");
+    }
 })
-
-
-
+app.patch("/user", async(req,res) => {
+    const firstName = req.body.firstName;
+    const body = req.body;
+    try{
+    const users = await User.findOneAndUpdate({firstName:firstName}, body,{
+      returnDocument:"after",
+      runValidators:true
+    })
+    res.send(users.firstName + " " + users.lastName)  
+    }catch(err){
+        res.status(404).send("update is unsuccessful:" + err.message);
+    }
+})
 
 connectDB()
-    .then(() => {
-        console.log("DB is connected")
-        app.listen(3000 , () => {
-        console.log("Server is listening on port 3000")
-        });
-}).catch(err=>{
-    console.error("Database cannot be connected")
-})
-
+  .then(() => {
+    console.log("DB is connected");
+    app.listen(3000, () => {
+      console.log("Server is listening on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected");
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -1,27 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const userSchema = Schema({
-    firstName : {
-        type: String
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+  },
+  emailId: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: /.+\@.+\..+/,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 4,
+  },
+  age: {
+    type: Number,
+    min: 18,
+  },
+  gender: {
+    type: String,
+    validate(value) {
+      if (!["male", "female", "others"].includes(value)) {
+        throw new Error("Gender data is not valid");
+      }
     },
-    lastName: {
-        type: String
-    },
-    emailId: {
-        type: String
-    },
-    password: {
-        type: String
-    },
-    age: {
-        type: Number
-    },
-    gender: {
-        type: String
-    }
+  },
+  photoUrl: {
+    type: String,
+    default:
+      "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541",
+  },
+  about: {
+    type: String,
+  },
+  skills: {
+    type: [String],
+  },
+},
 
-})
+{
+    timestamps: true
+});
 
 //mongoose model
 
