5a6b949c4011f4e8134b28038c0ec877976ad875
Shaira <mallari.shaira31@gmail.com>
2025-11-20 11:49:42 +0800

    create match schema + match route + match and unmatch api

---
+++ src/models/connection.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const { Schema , model } = mongoose;
+
+const connectionSchema = new Schema({
+    fromUserId: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    toUserId: {
+        type:Schema.Types.ObjectId,
+        required: true
+    },
+    status: {
+        type: String,
+        enum : {
+            values: ['like','pass','unmatched'],
+            message:'{VALUE} not valid'
+        },
+        required:true
+    }
+}, {timestamps:true});
+
+connectionSchema.index(
+    { fromUserId:1, toUserId:1},
+    {unique: true}
+)
+connectionSchema.pre("save", function(next){
+    const connection = this;
+    if(connection.fromUserId.equals(connection.toUserId)){
+        throw new Error("You cannot send connection request to yourself")
+    }
+   next(); 
+
+})
+
+const Connection = new model('connectionrequest',connectionSchema)
+
+module.exports= Connection;
